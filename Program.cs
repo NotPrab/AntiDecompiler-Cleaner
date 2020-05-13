b40cafbd45a2a69c51673b47d3ce605201e65755
Prab <54489997+NotPrab@users.noreply.github.com>
2020-05-13 16:03:35 +0700

    Updated
    Take an suggestion from Illuzion

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using dnlib.DotNet;
 using dnlib.DotNet.Writer;
 using dnlib.DotNet.Emit;
 using System.Linq;
-using System.IO;
 
 namespace AntiDecompiler_Cleaner {
     static class Program {
@@ -20,6 +19,7 @@ namespace AntiDecompiler_Cleaner {
             }
             CrashDnSpy(module);
             CrashDnSpy2(module);
+            CrashDnSpy3(module);
             var Path = module.Kind == ModuleKind.Dll ? args[0].Replace(".dll", "-Cleaned.dll") : args[0].Replace(".exe", "-Cleaned.exe");
             if (module.IsILOnly) {
                 var opts = new ModuleWriterOptions(module);
@@ -82,5 +82,29 @@ namespace AntiDecompiler_Cleaner {
                 }
             }
         }
+        // Support for Box
+        public static void CrashDnSpy3(ModuleDefMD module) {
+            foreach(var method in module.GetTypes().SelectMany(type => type.Methods)) {
+                if (!method.HasBody)
+                    continue;
+                if (!method.Body.HasInstructions)
+                    continue;
+                var instr = method.Body.Instructions;
+                for(int i = 0; i < instr.Count; i++) {
+                    try {
+                        if (instr[i].OpCode != OpCodes.Box)
+                            continue;
+                        instr[i].OpCode = OpCodes.Nop;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("[+] Cleaning AntiDeompiler method : 0x{0:x2} {1} ", method.MDToken.Raw, method.Name);
+                    }
+                    catch (Exception ex){
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[+] Failed to clean method {0}\n     {1}", method.Name, ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
+// this still need to improves a lot since it may cause errors.
